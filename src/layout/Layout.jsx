6368857b9590f2f2ecc1ea270ec84a757be47d03
PeppeTll <peppetll@gmail.com>
2023-09-06 17:31:03 +0200

    feat: footer component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,13 +2,14 @@ import styles from "./index.module.scss";
 
 //Components
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
 			{children}
-			{/* <Footer /> */}
+			<Footer />
 		</>
 	);
 };
