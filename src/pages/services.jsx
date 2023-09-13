6413ceea7472dd73d0be75a169728b52da5c22d6
PeppeTll <peppetll@gmail.com>
2023-09-13 16:28:06 +0200

    feat: pages

---
+++ src/pages/services.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/services.module.scss";
+
+function services() {
+	return (
+		<div className={styles.services}>
+			<p></p>
+		</div>
+	);
+}
+
+export default services;
